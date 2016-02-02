bb8972727a7c86984c7e3e32348a1dc317d544ff
Hǎiliàng Wáng <w@h12.me>
2016-02-02 19:28:05 +0800

    improve configuration & code generation

--- mongoschema.go
+++ mongoschema.go
@@ -3,102 +3,103 @@ package main
 import (
 	"bytes"
 	"errors"
-	"flag"
 	"fmt"
-	"go/format"
+	"io/ioutil"
+	"log"
 	"os"
 	"regexp"
+	"sort"
 	"strings"
 	"time"
 	"unicode"
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/yaml.v2"
 )
 
 var errEmptyURL = errors.New("mongoschema: no URL specified")
 
 func main() {
-	var generator Generator
-	flag.StringVar(&generator.URL, "url", "", "mongo url for dial")
-	flag.StringVar(&generator.DB, "db", "", "database to use")
-	flag.StringVar(&generator.Collection, "collection", "", "collection to use")
-	flag.StringVar(&generator.Struct, "struct", "", "name of the struct")
-	flag.StringVar(&generator.Package, "package", "", "pkg for the generate code")
-	flag.BoolVar(&generator.Raw, "raw", false, "output pre-gofmt code")
-	flag.BoolVar(&generator.Comments, "comments", true, "output comments in code")
-	flag.UintVar(&generator.Limit, "limit", 0, "maximum number of documents to scan")
-	ignoredKeys := flag.String("ignored-keys", "", "comma separated list of key names to ignore")
-	flag.Parse()
+	if len(os.Args) < 2 {
+		fmt.Println("mongoschema [config.yaml]")
+		return
+	}
 
-	generator.IgnoredKeys = strings.Split(*ignoredKeys, ",")
-
-	if err := generator.Generate(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(2)
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	buf, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var g Generator
+	if err := yaml.Unmarshal(buf, &g); err != nil {
+		log.Fatal(err)
+	}
+	if err := g.Generate(); err != nil {
+		log.Fatal(err)
 	}
 }
 
 type Generator struct {
-	URL         string
-	DB          string
-	Collection  string
-	Package     string
-	Struct      string
-	Raw         bool
-	Comments    bool
-	IgnoredKeys []string
-	Limit       uint
+	URL           string       `yaml:"url"`
+	DB            string       `yaml:"db"`
+	Limit         uint         `yaml:"limit"`
+	Comments      bool         `yaml:"comments"`
+	IgnoredFields []string     `yaml:"ignored_fields"`
+	Collections   []Collection `yaml:"collections"`
 }
 
-func (s *Generator) connect() (*mgo.Session, *mgo.Collection, error) {
+type Collection struct {
+	Name   string `yaml:"name"`
+	Struct string `yaml:"struct"`
+}
+
+func (s *Generator) connect() (*mgo.Session, error) {
 	if s.URL == "" {
-		return nil, nil, errEmptyURL
+		return nil, errEmptyURL
 	}
 
 	session, err := mgo.Dial(s.URL)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 	session.EnsureSafe(&mgo.Safe{})
 	session.SetBatch(1000)
 	session.SetMode(mgo.Eventual, true)
-	return session, session.DB(s.DB).C(s.Collection), nil
+	return session, nil
 }
 
 func (s *Generator) Generate() error {
-	session, collection, err := s.connect()
+	session, err := s.connect()
 	if err != nil {
 		return err
 	}
+	defer session.Close()
+	for _, c := range s.Collections {
+		collection := session.DB(s.DB).C(c.Name)
 
-	root := StructType{}
-	iter := collection.Find(nil).Iter()
-	m := bson.M{}
-	var seen uint
-	for iter.Next(m) {
-		if s.Limit != 0 && seen == s.Limit {
-			break
+		root := StructType{}
+		iter := collection.Find(nil).Iter()
+		m := bson.M{}
+		var seen uint
+		for iter.Next(m) {
+			if s.Limit != 0 && seen == s.Limit {
+				break
+			}
+			root.Merge(NewType(m, s), s)
+			m = bson.M{}
+			seen++
 		}
-		root.Merge(NewType(m, s), s)
-		m = bson.M{}
-		seen++
+		if err := iter.Close(); err != nil {
+			return err
+		}
+		fmt.Println(c.Struct, root.GoType(s))
+		fmt.Println()
 	}
-	if err := iter.Close(); err != nil {
-		return err
-	}
-	session.Close()
-
-	const srcFmt = "package %s\ntype %s %s"
-	src := fmt.Sprintf(srcFmt, s.Package, s.Struct, root.GoType(s))
-	if s.Raw {
-		fmt.Println(src)
-	}
-	formatted, err := format.Source([]byte(src))
-	if err != nil {
-		return err
-	}
-	fmt.Printf("%s\n", formatted)
 	return nil
 }
 
@@ -116,6 +117,12 @@ func (l LiteralType) GoType(gen *Generator) string {
 }
 
 func (l LiteralType) Merge(t Type, gen *Generator) Type {
+	if isNil(l) {
+		return t
+	}
+	if isNil(t) {
+		return l
+	}
 	if l.GoType(gen) == t.GoType(gen) {
 		return l
 	}
@@ -163,6 +170,7 @@ const (
 	PrimitiveObjectId
 	PrimitiveString
 	PrimitiveTimestamp
+	PrimitiveDBRef
 )
 
 func (p PrimitiveType) GoType(gen *Generator) string {
@@ -183,11 +191,32 @@ func (p PrimitiveType) GoType(gen *Generator) string {
 		return "time.Time"
 	case PrimitiveObjectId:
 		return "bson.ObjectId"
+	case PrimitiveDBRef:
+		return "mgo.DBRef"
 	}
 	panic(fmt.Sprintf("unknown primitive: %d", uint(p)))
 }
 
 func (p PrimitiveType) Merge(t Type, gen *Generator) Type {
+	if isNil(p) {
+		return t
+	}
+	if isNil(t) {
+		return p
+	}
+	switch p {
+	case PrimitiveInt32, PrimitiveInt64:
+		if t == PrimitiveDouble {
+			return PrimitiveDouble
+		}
+	}
+	switch t {
+	case PrimitiveInt32, PrimitiveInt64:
+		if p == PrimitiveDouble {
+			return PrimitiveDouble
+		}
+	}
+
 	if p.GoType(gen) == t.GoType(gen) {
 		return p
 	}
@@ -203,6 +232,12 @@ func (s SliceType) GoType(gen *Generator) string {
 }
 
 func (s SliceType) Merge(t Type, gen *Generator) Type {
+	if isNil(s) {
+		return t
+	}
+	if isNil(t) {
+		return s
+	}
 	if s.GoType(gen) == t.GoType(gen) {
 		return s
 	}
@@ -237,16 +272,19 @@ type StructType map[string]Type
 func (s StructType) GoType(gen *Generator) string {
 	var buf bytes.Buffer
 	fmt.Fprintln(&buf, "struct {")
-	for k, v := range s {
+	var keys sort.StringSlice
+	for k := range s {
+		if sscontains(gen.IgnoredFields, k) {
+			continue
+		}
+		keys = append(keys, k)
+	}
+	sort.Sort(keys)
+
+	for _, k := range keys {
+		v := s[k]
 		if isValidFieldName(k) {
-			var vGoType string
-
-			if sscontains(gen.IgnoredKeys, k) {
-				vGoType = "IgnoredField"
-			} else {
-				vGoType = v.GoType(gen)
-			}
-
+			vGoType := v.GoType(gen)
 			fmt.Fprintf(
 				&buf,
 				"%s %s `bson:\"%s,omitempty\"`\n",
@@ -265,6 +303,12 @@ func (s StructType) GoType(gen *Generator) string {
 }
 
 func (s StructType) Merge(t Type, gen *Generator) Type {
+	if isNil(t) {
+		return s
+	}
+	if isNil(s) {
+		return t
+	}
 	if o, ok := t.(StructType); ok {
 		for k, v := range o {
 			if e, ok := s[k]; ok {
@@ -290,12 +334,12 @@ func NewType(v interface{}, gen *Generator) Type {
 		return NewStructType(i, gen)
 	case []interface{}:
 		if len(i) == 0 {
-			return SliceType{Type: MixedType{}}
+			return SliceType{Type: NilType}
 		}
 		var s Type
 		for _, v := range i {
 			vt := NewType(v, gen)
-			if vt == NilType {
+			if isNil(vt) {
 				continue
 			}
 			if s == nil {
@@ -305,7 +349,7 @@ func NewType(v interface{}, gen *Generator) Type {
 			}
 		}
 		if s == nil {
-			return SliceType{Type: MixedType{}}
+			return SliceType{Type: NilType}
 		}
 		return s
 	case int, int64:
@@ -326,10 +370,13 @@ func NewType(v interface{}, gen *Generator) Type {
 }
 
 func NewStructType(m bson.M, gen *Generator) Type {
+	if m["$db"] != nil && m["$ref"] != nil && m["$id"] != nil {
+		return PrimitiveDBRef
+	}
 	s := StructType{}
 	for k, v := range m {
 		t := NewType(v, gen)
-		if t == NilType {
+		if isNil(t) {
 			continue
 		}
 		s[k] = t
@@ -390,4 +437,17 @@ func sscontains(l []string, v string) bool {
 		}
 	}
 	return false
+}
+
+func isNil(t Type) bool {
+	if t == NilType {
+		return true
+	}
+	if sliceType, ok := t.(SliceType); ok {
+		return isNil(sliceType.Type)
+	}
+	if mixedType, ok := t.(MixedType); ok {
+		return len(mixedType) > 0
+	}
+	return t == nil
 }
